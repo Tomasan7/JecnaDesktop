8c4d429314163c51bcc802b31b8203a5fd7f8bbd
Tomasan7 <tomashula06@gmail.com>
2022-03-16 19:55:06 +0100

    JavaFX setup

--- src/main/java/me/tomasan7/jecnadesktop/Main.java
+++ src/main/java/me/tomasan7/jecnadesktop/Main.java
@@ -1,9 +1,18 @@
 package me.tomasan7.jecnadesktop;
 
-public class Main
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application
 {
 	public static void main (String[] args)
 	{
-		System.out.println("Hello World");
+		Application.launch(args);
+	}
+
+	@Override
+	public void start (Stage primaryStage)
+	{
+		primaryStage.show();
 	}
 }
