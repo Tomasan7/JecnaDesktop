fe947c2c52f013fc9bbfb2185c215f710f28091a
Tomasan7 <tomashula06@gmail.com>
2022-03-20 19:32:33 +0100

    Created MainPage draft

---
+++ src/main/java/me/tomasan7/jecnadesktop/ui/controller/MainPageController.java
@@ -0,0 +1,13 @@
+package me.tomasan7.jecnadesktop.ui.controller;
+
+import me.tomasan7.jecnadesktop.Main;
+
+public class MainPageController
+{
+	private final Main main;
+
+	public MainPageController (Main main)
+	{
+		this.main = main;
+	}
+}
