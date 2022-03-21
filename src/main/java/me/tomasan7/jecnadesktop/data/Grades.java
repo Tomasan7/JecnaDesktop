2991e48e6944bfc09633ea62cb9d4efae360bbba
Tomasan7 <tomashula06@gmail.com>
2022-03-21 18:06:17 +0100

    Created builder for Grades class

--- src/main/java/me/tomasan7/jecnadesktop/data/Grades.java
+++ src/main/java/me/tomasan7/jecnadesktop/data/Grades.java
@@ -1,6 +1,9 @@
 package me.tomasan7.jecnadesktop.data;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -11,8 +14,24 @@ public record Grades(Map<String, List<Grade>> grades)
 		this(new HashMap<>());
 	}
 
+	public static Builder builder ()
+	{
+		return new Builder();
+	}
+
 	public static class Builder
 	{
-		// TODO: Implement.
+		private final Map<String, List<Grade>> grades = new HashMap<>();
+
+		public void addGrade (@NotNull String subject, @NotNull Grade grade)
+		{
+			/* Gets the list for the subject, if none is present, creates a new list and puts it into the map. Then the grade is added to that list. */
+			grades.computeIfAbsent(subject, k -> new LinkedList<>()).add(grade);
+		}
+
+		public Grades build ()
+		{
+			return new Grades(grades);
+		}
 	}
 }
