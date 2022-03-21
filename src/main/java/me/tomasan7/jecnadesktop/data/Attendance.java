8c5a8e0b4f9d4abf1ff7ee16b3fbd266670fc219
Tomasan7 <tomashula06@gmail.com>
2022-03-21 18:06:50 +0100

    Added @NotNull to Attendance parameter

--- src/main/java/me/tomasan7/jecnadesktop/data/Attendance.java
+++ src/main/java/me/tomasan7/jecnadesktop/data/Attendance.java
@@ -1,11 +1,13 @@
 package me.tomasan7.jecnadesktop.data;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.time.LocalDateTime;
 
 /**
  * @param exit Whether the person exited or entered. False for enter. True for exit.
  * @param time When the person entered/exited.
  */
-public record Attendance(boolean exit, LocalDateTime time)
+public record Attendance(boolean exit, @NotNull LocalDateTime time)
 {
 }
