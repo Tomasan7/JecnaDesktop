b8416ad3831620ca0fbb3969fe3ef26fd41abaa5
Tomasan7 <tomashula06@gmail.com>
2022-03-24 20:15:25 +0100

    Improved Attendances and made them to better reflect actual situation
    Attencances weren't able to represent more enters/leaves in one day, which is possible.

--- src/main/java/me/tomasan7/jecnadesktop/data/Attendance.java
+++ src/main/java/me/tomasan7/jecnadesktop/data/Attendance.java
@@ -3,11 +3,12 @@ package me.tomasan7.jecnadesktop.data;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 /**
- * @param exit Whether the person exited or entered. False for enter. True for exit.
+ * @param exit Whether the person exited or entered. False for enter, true for exit.
  * @param time When the person entered/exited.
  */
-public record Attendance(boolean exit, @NotNull LocalDateTime time)
+public record Attendance(boolean exit, @NotNull LocalTime time)
 {
 }
--- src/main/java/me/tomasan7/jecnadesktop/data/Attendances.java
+++ src/main/java/me/tomasan7/jecnadesktop/data/Attendances.java
@@ -1,17 +1,66 @@
 package me.tomasan7.jecnadesktop.data;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.time.LocalDate;
+import java.util.*;
 
-public record Attendances(Set<Attendance> attendances) implements Iterable<Attendance>
+public class Attendances
 {
-	/**
-	 * Returns immutable iterator.
-	 */
-	@Override
-	public Iterator<Attendance> iterator ()
+	private final Map<LocalDate, List<Attendance>> attendances;
+
+	public Attendances (Map<LocalDate, List<Attendance>> attendances)
 	{
-		return new HashSet<>(attendances).iterator();
+		/* Encapsulation. */
+		this.attendances = new HashMap<>(attendances);
+	}
+
+	/**
+	 * Returns all {@link Attendance attendances} for provided day.
+	 * @param day The day to get all {@link Attendance attendances} from.
+	 * @return All the {@link Attendance attendances} for the day.
+	 */
+	public List<Attendance> getAttendancesForDay (LocalDate day)
+	{
+		/* Return empty list when no data for the passed day is present. */
+		if (!attendances.containsKey(day))
+			return new ArrayList<>();
+
+		/* Encapsulation. */
+		return new ArrayList<>(attendances.get(day));
+	}
+
+	/**
+	 * Returns all the days.
+	 */
+	public Set<LocalDate> getDays ()
+	{
+		/* Encapsulation */
+		return new HashSet<>(attendances.keySet());
+	}
+
+	public static Builder builder ()
+	{
+		return new Builder();
+	}
+
+	public static class Builder
+	{
+		private final Map<LocalDate, List<Attendance>> attendances = new HashMap<>();
+
+		/**
+		 * Adds {@link Attendance}.
+		 * @param day The day to add this attendance to.
+		 * @param attendance The {@link Attendance} to add.
+		 */
+		public Builder addAttendance (LocalDate day, Attendance attendance)
+		{
+			/* Gets the list for the day, if none is present, creates a new list and puts it into the map. Then the attendance is added to that list. */
+			this.attendances.computeIfAbsent(day, __ -> new ArrayList<>()).add(attendance);
+			return this;
+		}
+
+		public Attendances build ()
+		{
+			return new Attendances(attendances);
+		}
 	}
 }
