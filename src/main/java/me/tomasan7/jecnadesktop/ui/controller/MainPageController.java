50589644061cdf899a2adfad03c90d4c5f1c7beb
Tomasan7 <tomashula06@gmail.com>
2022-04-12 20:20:15 +0200

    SideBarElements now change style state when selected
    Now you can target SideBarElement with :selected state in css.

--- src/main/java/me/tomasan7/jecnadesktop/ui/controller/MainPageController.java
+++ src/main/java/me/tomasan7/jecnadesktop/ui/controller/MainPageController.java
@@ -1,5 +1,6 @@
 package me.tomasan7.jecnadesktop.ui.controller;
 
+import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
@@ -7,6 +8,7 @@ import me.tomasan7.jecnadesktop.JecnaDesktop;
 import me.tomasan7.jecnadesktop.ui.component.SideBarElement;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainPageController implements Initializable
@@ -24,12 +26,27 @@ public class MainPageController implements Initializable
 	@Override
 	public void initialize (URL location, ResourceBundle resources)
 	{
-		sidebar.getChildrenUnmodifiable().forEach(child ->
-		{
-			if (!(child instanceof SideBarElement sideBarElement))
-				return;
+		initSideBarElements();
+	}
 
-			sideBarElement.setOnMouseClicked(event -> jecnaDesktop.getSubPageManager().switchToPage(sideBarElement.getSubPage()));
-		});
+	private void initSideBarElements ()
+	{
+		/* Get the SideBarElements from the sidebar. */
+		List<SideBarElement> sideBarElements = sidebar.getChildrenUnmodifiable().stream()
+													  .filter(child -> child instanceof SideBarElement)
+													  .map(child -> (SideBarElement) child)
+													  .toList();
+
+		sideBarElements.forEach(sideBarElement ->
+				sideBarElement.setOnMouseClicked(event ->
+				{
+					/* Switch to SideBarElement's subpage. */
+					jecnaDesktop.getSubPageManager().switchToPage(sideBarElement.getSubPage());
+
+					/* Edit the modified value of each SideBarElement based on whether it's subpage is the current active one or not. */
+					sideBarElements.forEach(sbe ->
+							sbe.pseudoClassStateChanged(PseudoClass.getPseudoClass("selected"),
+									jecnaDesktop.getSubPageManager().getCurrentPage() == sbe.getSubPage()));
+				}));
 	}
 }
