efdf83833a8592d27ea529e304d679d4a6c6db47
Tomasan7 <tomashula06@gmail.com>
2022-04-12 20:05:48 +0200

    Implemented auth storing in LoginController

--- src/main/java/me/tomasan7/jecnadesktop/ui/controller/LoginPageController.java
+++ src/main/java/me/tomasan7/jecnadesktop/ui/controller/LoginPageController.java
@@ -9,11 +9,14 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import me.tomasan7.jecnadesktop.JecnaDesktop;
 import me.tomasan7.jecnadesktop.ui.JDScene;
+import me.tomasan7.jecnadesktop.ui.SubPage;
+import me.tomasan7.jecnadesktop.util.AuthStore;
 import me.tomasan7.jecnadesktop.web.Auth;
 import me.tomasan7.jecnadesktop.web.JecnaWebClient;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Supplier;
 
 public class LoginPageController implements Initializable
 {
@@ -34,15 +37,30 @@ public class LoginPageController implements Initializable
 	@Override
 	public void initialize (URL location, ResourceBundle resources)
 	{
-		/* Disables/enables the login button based on whether both the fields are filled or not.  */
-		ChangeListener<String> changeListener = (observable, oldValue, newValue) ->
+		if (AuthStore.isSaved())
+		{
+			Auth auth = AuthStore.load();
+			JecnaWebClient jecnaWebClient = new JecnaWebClient(auth);
+
+			jecnaWebClient.login().thenAccept(successful ->
+			{
+				if (successful)
+					continueToMain(jecnaWebClient);
+			});
+		}
+
+		/* Whether the user can hit login or not. User can log in, when neither field is blank. */
+		Supplier<Boolean> canlogin = () ->
 		{
 			/* If neither is blank. */
 			if (!passwordInput.textProperty().get().isBlank() && !userInput.textProperty().get().isBlank())
-				loginBtn.setDisable(false);
+				return true;
 			else /* Both are blank. */
-				loginBtn.setDisable(true);
+				return false;
 		};
+
+		/* Disables/enables the login button based on whether both the fields are filled or not.  */
+		ChangeListener<String> changeListener = (__, ___, ____) -> loginBtn.setDisable(!canlogin.get());
 
 		passwordInput.textProperty().addListener(changeListener);
 		userInput.textProperty().addListener(changeListener);
@@ -50,24 +68,61 @@ public class LoginPageController implements Initializable
 		/* Run when you either press the login button or hit enter while the password field is focused. */
 		Runnable onLogin = () ->
 		{
-			JecnaWebClient jecnaWebClient = new JecnaWebClient(new Auth(userInput.getText(), passwordInput.getText()));
+			Auth auth = null;
 
+			try
+			{
+				auth = new Auth(userInput.getText(), passwordInput.getText());
+			}
+			catch (IllegalArgumentException e)
+			{
+				clearFields();
+				return;
+			}
+
+			JecnaWebClient jecnaWebClient = new JecnaWebClient(auth);
+
+			Auth finalAuth = auth;
 			jecnaWebClient.login().thenAccept(successful ->
 			{
 				if (successful)
 				{
-					jecnaDesktop.initDataAccess(jecnaWebClient);
-					Platform.runLater(() -> jecnaDesktop.getSceneManager().switchToScene(JDScene.MAIN));
+					if (!AuthStore.isSaved())
+						AuthStore.save(finalAuth);
+
+					continueToMain(jecnaWebClient);
 				}
 				else
-				{
-					userInput.clear();
-					passwordInput.clear();
-				}
+					clearFields();
 			});
 		};
 
-		passwordInput.setOnAction(event -> onLogin.run());
-		loginBtn.setOnMouseClicked(event -> onLogin.run());
+		passwordInput.setOnAction(event -> {if (canlogin.get()) onLogin.run();});
+		loginBtn.setOnMouseClicked(event -> {if (canlogin.get()) onLogin.run();});
+	}
+
+	private void clearFields ()
+	{
+		userInput.clear();
+		passwordInput.clear();
+	}
+
+	private void continueToMain (JecnaWebClient jecnaWebClient)
+	{
+		jecnaDesktop.initDataAccess(jecnaWebClient);
+
+		if (!Platform.isFxApplicationThread())
+		{
+			Platform.runLater(() ->
+			{
+				jecnaDesktop.getSceneManager().switchToScene(JDScene.MAIN);
+				jecnaDesktop.getSubPageManager().switchToPage(SubPage.ATTENDANCES);
+			});
+		}
+		else
+		{
+			jecnaDesktop.getSceneManager().switchToScene(JDScene.MAIN);
+			jecnaDesktop.getSubPageManager().switchToPage(SubPage.ATTENDANCES);
+		}
 	}
 }
