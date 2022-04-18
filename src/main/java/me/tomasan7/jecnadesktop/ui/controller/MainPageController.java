62264009532f0a09f8a9a0c9ee6fb3f35ecf1088
Tomasan7 <tomashula06@gmail.com>
2022-04-18 09:26:02 +0200

    Made SideBarElement :selected change by listening to SubPage switching
    Before it was chenged when you clicked on the SideBar.
    So when you changed the SubPage programatically, the selected pseudo-class wouldn't change.

--- src/main/java/me/tomasan7/jecnadesktop/ui/controller/MainPageController.java
+++ src/main/java/me/tomasan7/jecnadesktop/ui/controller/MainPageController.java
@@ -1,5 +1,6 @@
 package me.tomasan7.jecnadesktop.ui.controller;
 
+import javafx.application.Platform;
 import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,6 +11,8 @@ import me.tomasan7.jecnadesktop.ui.component.SideBarElement;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MainPageController implements Initializable
 {
@@ -38,15 +41,22 @@ public class MainPageController implements Initializable
 													  .toList();
 
 		sideBarElements.forEach(sideBarElement ->
-				sideBarElement.setOnMouseClicked(event ->
+				sideBarElement.setOnMouseClicked(__ ->
 				{
 					/* Switch to SideBarElement's subpage. */
 					jecnaDesktop.getSubPageManager().switchToPage(sideBarElement.getSubPage());
-
-					/* Edit the modified value of each SideBarElement based on whether it's subpage is the current active one or not. */
-					sideBarElements.forEach(sbe ->
-							sbe.pseudoClassStateChanged(PseudoClass.getPseudoClass("selected"),
-									jecnaDesktop.getSubPageManager().getCurrentPage() == sbe.getSubPage()));
 				}));
+
+		/* We need to set the listener later, because SubPageManager isn't initialized yet.
+		* This is kind of a hack, should be change to make it not hacky. */
+		Executors.newSingleThreadScheduledExecutor().schedule(() ->
+				Platform.runLater(() ->
+						jecnaDesktop.getSubPageManager().registerSubPageSwitchListener(((oldSubPage, newSubPage) ->
+						{
+							/* Edit the modified value of each SideBarElement based on whether it's subpage is the current active one or not. */
+							sideBarElements.forEach(sbe ->
+									sbe.pseudoClassStateChanged(PseudoClass.getPseudoClass("selected"),
+											sbe.getSubPage() == newSubPage));
+						}))), 10, TimeUnit.MILLISECONDS);
 	}
 }
