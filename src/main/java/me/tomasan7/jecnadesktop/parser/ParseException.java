2ebc860cd97b020ac095642a3e24dab19d393838
Tomasan7 <tomashula06@gmail.com>
2022-04-21 18:15:31 +0200

    Added cause constructors to ParseException

--- src/main/java/me/tomasan7/jecnadesktop/parser/ParseException.java
+++ src/main/java/me/tomasan7/jecnadesktop/parser/ParseException.java
@@ -10,4 +10,14 @@ public class ParseException extends RuntimeException
 	{
 		super(message);
 	}
+
+	public ParseException (String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public ParseException (Throwable cause)
+	{
+		super(cause);
+	}
 }
