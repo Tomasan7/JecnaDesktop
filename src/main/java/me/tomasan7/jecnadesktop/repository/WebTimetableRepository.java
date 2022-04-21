4eaf5fc7a8926660f316a8c014269409db6e107c
Tomasan7 <tomashula06@gmail.com>
2022-04-21 19:05:14 +0200

    Created TimetableRepository and WebTimetableRepository implementation

---
+++ src/main/java/me/tomasan7/jecnadesktop/repository/WebTimetableRepository.java
@@ -0,0 +1,41 @@
+package me.tomasan7.jecnadesktop.repository;
+
+import me.tomasan7.jecnadesktop.data.Timetable;
+import me.tomasan7.jecnadesktop.parser.parsers.HTMLTimetableParser;
+import me.tomasan7.jecnadesktop.web.JecnaWebClient;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class WebTimetableRepository implements TimetableRepository
+{
+	private static final String WEB_PATH = "/timetable/class";
+
+	private final JecnaWebClient webClient;
+	private final HTMLTimetableParser timetableParser = new HTMLTimetableParser();
+
+	public WebTimetableRepository (JecnaWebClient webClient)
+	{
+		this.webClient = webClient;
+	}
+
+	@Override
+	public Timetable queryTimetable ()
+	{
+		try
+		{
+			return timetableParser.parse(webClient.query(WEB_PATH).get());
+		}
+		catch (ExecutionException | InterruptedException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public CompletableFuture<Timetable> queryTimetableAsync ()
+	{
+		return webClient.query(WEB_PATH)
+						.thenApply(timetableParser::parse);
+	}
+}
