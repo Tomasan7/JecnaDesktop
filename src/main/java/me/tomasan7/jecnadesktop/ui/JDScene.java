aa0b80b5e025a2f51d038686eed9d4cca1602c3d
Tomasan7 <tomashula06@gmail.com>
2022-04-23 09:58:50 +0200

    Made it possible so a JDScene can be without controller

--- src/main/java/me/tomasan7/jecnadesktop/ui/JDScene.java
+++ src/main/java/me/tomasan7/jecnadesktop/ui/JDScene.java
@@ -5,6 +5,7 @@ import javafx.scene.Parent;
 import me.tomasan7.jecnadesktop.JecnaDesktop;
 import me.tomasan7.jecnadesktop.ui.controller.LoginPageController;
 import me.tomasan7.jecnadesktop.ui.controller.MainPageController;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.util.function.Function;
@@ -12,7 +13,8 @@ import java.util.function.Function;
 public enum JDScene
 {
 	LOGIN("/ui/scene/LoginPage.fxml", LoginPageController::new),
-	MAIN("/ui/scene/MainPage.fxml", MainPageController::new);
+	MAIN("/ui/scene/MainPage.fxml", MainPageController::new),
+	LOADING("/ui/scene/LoadingPage.fxml", null);
 
 	/**
 	 * The string location of the FXML file inside resources.
@@ -21,9 +23,10 @@ public enum JDScene
 	/**
 	 * A function to create new instance of the FXML's controller.
 	 */
+	@Nullable
 	private final Function<JecnaDesktop, Object> newController;
 
-	JDScene (String location, Function<JecnaDesktop, Object> newController)
+	JDScene (String location, @Nullable Function<JecnaDesktop, Object> newController)
 	{
 		this.location = location;
 		this.newController = newController;
@@ -39,23 +42,28 @@ public enum JDScene
 
 	/**
 	 * Returns new instance of this scene's controller.
+	 *
 	 * @param jecnaDesktop The {@link JecnaDesktop} instance.
-	 * @return The new controller.
+	 * @return The new controller. Or {@code null}, if there's no controller.
 	 */
+	@Nullable
 	public Object newController (JecnaDesktop jecnaDesktop)
 	{
-		return newController.apply(jecnaDesktop);
+		return newController != null ? newController.apply(jecnaDesktop) : null;
 	}
 
 	/**
 	 * Returns new {@link Parent page's root}.
+	 *
 	 * @param jecnaDesktop The {@link JecnaDesktop} instance.
 	 * @return The new {@link Parent page's root}.
 	 */
 	public Parent loadPageRoot (JecnaDesktop jecnaDesktop)
 	{
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(location));
-		fxmlLoader.setController(newController(jecnaDesktop));
+		Object controller = newController(jecnaDesktop);
+		if (controller != null)
+			fxmlLoader.setController(controller);
 
 		Parent page = null;
 
