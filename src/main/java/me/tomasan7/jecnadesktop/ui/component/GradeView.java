ac1833fab1df4290d96be5ebe8deca2f24cf2ae0
Tomasan7 <tomashula06@gmail.com>
2022-04-24 17:31:17 +0200

    Added tooltip for GradeView showing the grade's description and date

--- src/main/java/me/tomasan7/jecnadesktop/ui/component/GradeView.java
+++ src/main/java/me/tomasan7/jecnadesktop/ui/component/GradeView.java
@@ -1,10 +1,13 @@
 package me.tomasan7.jecnadesktop.ui.component;
 
+import io.github.palexdev.materialfx.controls.MFXTooltip;
 import javafx.css.PseudoClass;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import me.tomasan7.jecnadesktop.data.Grade;
+
+import java.time.format.DateTimeFormatter;
 
 public class GradeView extends AnchorPane
 {
@@ -13,6 +16,16 @@ public class GradeView extends AnchorPane
 	public GradeView (Grade grade)
 	{
 		this.grade = grade;
+
+		MFXTooltip tooltip = new MFXTooltip(this, String.format("%s (%s)",
+																grade.description() != null
+																		? grade.description()
+																		: "",
+																grade.receiveDate() != null
+																		? grade.receiveDate().format(DateTimeFormatter.ofPattern("d.M.yyyy"))
+																		: ""));
+		tooltip.getStyleSheets().add("/ui/ToolTip.css");
+		tooltip.install();
 
 		Label gradeLabel = new Label(String.valueOf(grade.valueChar()));
 
