92b48cf6d6fd140e9d459d9ab6256f2d05cf32de
Tomasan7 <tomashula06@gmail.com>
2022-05-24 15:04:14 +0200

    Convert AuthStore and LoginPageController

---
+++ app/src/main/kotlin/me/tomasan7/jecnadesktop/ui/controller/LoginPageController.kt
@@ -0,0 +1,126 @@
+package me.tomasan7.jecnadesktop.ui.controller
+
+import io.github.palexdev.materialfx.controls.MFXPasswordField
+import io.github.palexdev.materialfx.controls.MFXTextField
+import javafx.application.Platform
+import javafx.beans.value.ChangeListener
+import javafx.event.EventHandler
+import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.Button
+import me.tomasan7.jecnadesktop.JecnaDesktop
+import me.tomasan7.jecnadesktop.ui.JDScene
+import me.tomasan7.jecnadesktop.util.AuthStore
+import me.tomasan7.jecnadesktop.web.Auth
+import me.tomasan7.jecnadesktop.web.JecnaWebClient
+import java.net.URL
+import java.util.*
+
+class LoginPageController(private val jecnaDesktop: JecnaDesktop) : Initializable
+{
+    @FXML
+    private val userInput: MFXTextField? = null
+
+    @FXML
+    private val passwordInput: MFXPasswordField? = null
+
+    @FXML
+    private val loginBtn: Button? = null
+
+    override fun initialize(location: URL, resources: ResourceBundle)
+    {
+        if (AuthStore.isSaved)
+        {
+            val auth = AuthStore.load()
+
+            if (auth != null)
+            {
+                val jecnaWebClient = JecnaWebClient(auth)
+
+                jecnaDesktop.sceneManager!!.switchToScene(JDScene.LOADING)
+                jecnaWebClient.login().thenAccept { successful ->
+
+                    if (successful)
+                        continueToMain(jecnaWebClient)
+                    else
+                    {
+                        AuthStore.delete()
+                        Platform.runLater { jecnaDesktop.sceneManager!!.switchToScene(JDScene.LOGIN) }
+                    }
+                }
+            }
+            else
+                AuthStore.delete()
+        }
+
+        /* Disables/enables the login button based on whether both the fields are filled or not.  */
+        val changeListener = ChangeListener<String> { _, _, _ -> loginBtn!!.isDisable = !canLogin() }
+
+        passwordInput!!.textProperty().addListener(changeListener)
+        userInput!!.textProperty().addListener(changeListener)
+
+        passwordInput.onAction = EventHandler { if (canLogin()) onLogin() }
+        loginBtn!!.onMouseClicked = EventHandler { if (canLogin()) onLogin() }
+    }
+
+    /**
+     * @return Whether the user can hit login or not. User can log in, when neither field is blank.
+     */
+    private fun canLogin(): Boolean
+    {
+        /* If neither is blank. */
+        return passwordInput!!.text.isNotBlank() && userInput!!.text.isNotBlank()
+    }
+
+    /**
+     * Run when you either press the login button or hit enter while the password field is focused.
+     */
+    private fun onLogin()
+    {
+        val auth = try
+        {
+            Auth(userInput!!.text, passwordInput!!.text)
+        }
+        catch (e: IllegalArgumentException)
+        {
+            clearFields()
+            return
+        }
+
+        jecnaDesktop.sceneManager!!.switchToScene(JDScene.LOADING)
+
+        val jecnaWebClient = JecnaWebClient(auth)
+
+        jecnaWebClient.login().thenAccept { successful: Boolean ->
+
+            if (successful)
+            {
+                if (!AuthStore.isSaved)
+                    AuthStore.save(auth)
+
+                continueToMain(jecnaWebClient)
+            }
+            else
+            {
+                Platform.runLater { jecnaDesktop.sceneManager!!.switchToScene(JDScene.LOGIN) }
+                clearFields()
+            }
+        }
+    }
+
+    private fun clearFields()
+    {
+        userInput!!.clear()
+        passwordInput!!.clear()
+    }
+
+    private fun continueToMain(jecnaWebClient: JecnaWebClient)
+    {
+        jecnaDesktop.initDataAccess(jecnaWebClient)
+
+        if (!Platform.isFxApplicationThread())
+            Platform.runLater { jecnaDesktop.sceneManager!!.switchToScene(JDScene.MAIN) }
+        else
+            jecnaDesktop.sceneManager!!.switchToScene(JDScene.MAIN)
+    }
+}
---
+++ app/src/main/kotlin/me/tomasan7/jecnadesktop/util/AuthStore.kt
@@ -0,0 +1,76 @@
+package me.tomasan7.jecnadesktop.util
+
+import me.tomasan7.jecnadesktop.web.Auth
+import me.tomasan7.jecnadesktop.web.Auth.Companion.decrypt
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+
+/**
+ * Is responsible for saving and loading user's [Auth] to and from a file.
+ */
+object AuthStore
+{
+    private val FILE = File("auth.nggyu")
+
+    /**
+     * Saves the [Auth] to the file.
+     * @param auth The [Auth] to save.
+     * @param override Whether to override existing file or not.
+     */
+    fun save(auth: Auth, override: Boolean = true)
+    {
+        if (FILE.exists() && !override)
+            return
+
+        try
+        {
+            FileOutputStream(FILE).use { outputStream -> outputStream.write(auth.encrypt()) }
+        }
+        catch (e: IOException)
+        {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * Loads the [Auth] from the file.
+     * @return The loaded [Auth]. Or `null`, if the [Auth] isn't saved, or is incorrect format.
+     */
+	fun load(): Auth?
+    {
+        if (!isSaved) return null
+
+        try
+        {
+            FileInputStream(FILE).use { inputStream -> return decrypt(inputStream.readAllBytes()) }
+        }
+        catch (e: IOException)
+        {
+            e.printStackTrace()
+        }
+        catch (e: IllegalArgumentException)
+        {
+            /* Thrown when the saved password is in incorrect format. Should only happen, if the file was manipulated with from outside. */
+            return null
+        }
+
+        return null
+    }
+
+    /**
+     * @return Whether a file with saved [Auth] already exists or not.
+     */
+    val isSaved: Boolean
+        get() = FILE.exists()
+
+    /**
+     * Deletes the saved [Auth].
+     * @return `true`, if [Auth] was successfully deleted.
+     */
+	fun delete(): Boolean
+    {
+        return FILE.delete()
+    }
+}
