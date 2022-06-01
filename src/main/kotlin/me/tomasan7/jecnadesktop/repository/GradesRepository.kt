296c6a25146a69d1df8a11a9772039f8d104129a
Tomasan7 <tomashula06@gmail.com>
2022-06-01 20:56:16 +0200

    Implement SchoolYear and school year half to GradesRepository

--- src/main/kotlin/me/tomasan7/jecnadesktop/repository/GradesRepository.kt
+++ src/main/kotlin/me/tomasan7/jecnadesktop/repository/GradesRepository.kt
@@ -1,6 +1,7 @@
 package me.tomasan7.jecnadesktop.repository
 
 import me.tomasan7.jecnadesktop.data.Grades
+import me.tomasan7.jecnadesktop.data.SchoolYear
 
 /**
  * Retrieves [Grades] from any kind of data source.
@@ -8,4 +9,10 @@ import me.tomasan7.jecnadesktop.data.Grades
 interface GradesRepository
 {
     suspend fun queryGrades(): Grades
+
+    /**
+     * @param schoolYear The [SchoolYear] to get the [Grades] for.
+     * @param firstHalf The school year half to get the [Grades] for. `true` for first half and `false` for second half.
+     */
+    suspend fun queryGrades(schoolYear: SchoolYear, firstHalf: Boolean): Grades
 }
--- src/main/kotlin/me/tomasan7/jecnadesktop/repository/WebGradesRepository.kt
+++ src/main/kotlin/me/tomasan7/jecnadesktop/repository/WebGradesRepository.kt
@@ -1,8 +1,13 @@
 package me.tomasan7.jecnadesktop.repository
 
+import io.ktor.http.*
 import me.tomasan7.jecnadesktop.data.Grades
+import me.tomasan7.jecnadesktop.data.SchoolYear
 import me.tomasan7.jecnadesktop.parser.parsers.HtmlGradesParser
+import me.tomasan7.jecnadesktop.util.JecnaPeriodEncoder
+import me.tomasan7.jecnadesktop.util.JecnaPeriodEncoder.jecnaEncode
 import me.tomasan7.jecnadesktop.web.JecnaWebClient
+import me.tomasan7.jecnadesktop.web.append
 
 /**
  * Retrieves [Grades] from the Ječná web.
@@ -12,6 +17,11 @@ class WebGradesRepository(private val webClient: JecnaWebClient) : GradesReposit
     private val gradesParser = HtmlGradesParser()
 
     override suspend fun queryGrades() = gradesParser.parse(webClient.queryStringBody(WEB_PATH))
+
+    override suspend fun queryGrades(schoolYear: SchoolYear, firstHalf: Boolean) = gradesParser.parse(webClient.queryStringBody(WEB_PATH, Parameters.build {
+        append(schoolYear.jecnaEncode())
+        append(JecnaPeriodEncoder.encodeSchoolYearHalf(firstHalf))
+    }))
 
     companion object
     {
