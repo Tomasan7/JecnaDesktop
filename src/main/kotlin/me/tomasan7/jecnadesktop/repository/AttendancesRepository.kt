835990d4ac187e9e3305c5e822ec61c97dfb09e8
Tomasan7 <tomashula06@gmail.com>
2022-06-02 18:48:42 +0200

    Implement periods in AttendancesRepository

--- src/main/kotlin/me/tomasan7/jecnadesktop/repository/AttendancesRepository.kt
+++ src/main/kotlin/me/tomasan7/jecnadesktop/repository/AttendancesRepository.kt
@@ -1,6 +1,7 @@
 package me.tomasan7.jecnadesktop.repository
 
 import me.tomasan7.jecnadesktop.data.Attendances
+import me.tomasan7.jecnadesktop.data.SchoolYear
 
 /**
  * Retrieves [Attendances] from any kind of data source.
@@ -8,4 +9,10 @@ import me.tomasan7.jecnadesktop.data.Attendances
 interface AttendancesRepository
 {
     suspend fun queryAttendances(): Attendances
+
+    /**
+     * @param schoolYear The [SchoolYear] to get the [Attendances] for.
+     * @param month The month to get the [Attendances] for. (`1` = January)
+     */
+    suspend fun queryAttendances(schoolYear: SchoolYear, month: Int): Attendances
 }
